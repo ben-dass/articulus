1e0fd419805f8e3afd78392a57797bae6390d33e
Benjamin Dass <65924137+ben-dass@users.noreply.github.com>
2025-10-14 11:02:00 -0400

    CBVs for CRUD

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from app.models import Article
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "app/home.html", {"articles": articles})
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
 
 
 class ArticleCreateView(CreateView):
@@ -16,24 +17,16 @@ class ArticleCreateView(CreateView):
     success_url = reverse_lazy("home")
 
 
-# from app.forms import CreateArticleForm
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("home")
+    context_object_name = "articles"
 
-# def create_article(request):
-#     if request.method == "POST":
-#         form = CreateArticleForm(request.POST)
-#         if form.is_valid():
-#             form_data = form.cleaned_data
-#             new_article = Article(
-#                 title=form_data["title"],
-#                 status=form_data["status"],
-#                 content=form_data["content"],
-#                 word_count=form_data["word_count"],
-#                 twitter_post=form_data["twitter_post"],
-#             )
-#             new_article.save()
 
-#             return redirect("home")
-#     else:
-#         form = CreateArticleForm()
-
-#     return render(request, "app/article_create.html", {"form": form})
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
